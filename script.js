650cf42c50865fef190287708d829ee5a26124c9
Sayyeda Afshan <syed1995.sa@gmail.com>
2024-01-16 21:14:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function convertToUpperCase() {
+      var inputElement = document.getElementById("fname");
+      inputElement.value = inputElement.value.toUpperCase();
+    }
+
+    // Attach the event listener to the input field
+    document.getElementById("fname").addEventListener("blur", convertToUpperCase);
